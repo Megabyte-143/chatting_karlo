361286fa51f6ab7c30888c91cda7e8e9cc9fa39c
Megabyte <yashygkf@gmail.com>
2021-07-12 20:30:37 +0530

    Completed transfering from Auth to chat screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 import './screens/chat_screen.dart';
 import './screens/auth_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Chatting Karlo',
-      theme: ThemeData(
-        primarySwatch: Colors.pink,
-        backgroundColor: Colors.pink,
-        accentColor: Colors.deepPurple,
-        accentColorBrightness: Brightness.dark,
-        buttonTheme: ButtonTheme.of(context).copyWith(
-          buttonColor: Colors.pink,
-          textTheme: ButtonTextTheme.primary,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20),
-          ),
-        ),
-      ),
-      home: AuthScreen(),
-    );
+    return FutureBuilder(
+        future: _initialization,
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return MaterialApp(
+                title: 'Chatting Karlo',
+                theme: ThemeData(
+                  primarySwatch: Colors.pink,
+                  backgroundColor: Colors.pink,
+                  accentColor: Colors.deepPurple,
+                  accentColorBrightness: Brightness.dark,
+                  buttonTheme: ButtonTheme.of(context).copyWith(
+                    buttonColor: Colors.pink,
+                    textTheme: ButtonTextTheme.primary,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                ),
+                home: StreamBuilder(
+                  stream: FirebaseAuth.instance.authStateChanges(),
+                  builder: (ctx, userState) {
+                    if (userState.hasData) {
+                      return ChatScreen();
+                    }
+                    return AuthScreen();
+                  },
+                ));
+          }
+          return CircularProgressIndicator();
+        });
   }
 }
--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,14 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    Firebase.initializeApp();
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Chatting Karlo"),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: DropdownButton(
+              items: [
+                DropdownMenuItem(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.grey,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        Icon(
+                          Icons.exit_to_app,
+                          color: Theme.of(context).copyWith().primaryColor,
+                        ),
+                        //SizedBox(width: 40,),
+                        Text("Logout"),
+                      ],
+                    ),
+                  ),
+                  value: 'logout',
+                ),
+              ],
+              onChanged: (value) {
+                if (value == 'logout') {
+                  FirebaseAuth.instance.signOut();
+                }
+              },
+              icon: Icon(
+                Icons.more_vert,
+                color: Colors.yellow,
+              ),
+            ),
+          )
+        ],
+      ),
       body: StreamBuilder<QuerySnapshot>(
           stream: FirebaseFirestore.instance
               .collection('chats/JJJ1icBKXTftuip3wpb6/messages')
@@ -33,7 +73,7 @@ class ChatScreen extends StatelessWidget {
         onPressed: () {
           FirebaseFirestore.instance
               .collection('chats/JJJ1icBKXTftuip3wpb6/messages')
-              .add({'text': "Added by clicking button"}); 
+              .add({'text': "Added by clicking button"});
         },
       ),
     );
